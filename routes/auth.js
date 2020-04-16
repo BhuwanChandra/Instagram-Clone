300cbb77fa9ccd930db8a0a00b8656201d1ba9e0
BhuwanChandra <bhuwan7779@gmail.com>
2020-04-16 19:52:43 +0530

    implemented authentication

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../keys");
+const requireLogin = require('../middleware/requireLogin');
 
 router.get("/", (req, res) => {
   res.send("<h1> Hello </h1>");
@@ -76,5 +77,9 @@ router.post("/signin", (req, res) => {
     })
     .catch(err => console.log(err));
 });
+
+router.get('/user',requireLogin,(req, res) => {
+  res.send("Acthenticated user!!!");
+})
 
 module.exports = router;
