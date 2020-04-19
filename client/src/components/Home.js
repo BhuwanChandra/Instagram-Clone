372157b3ea8510e8eaa1d8a1873c9ee0fe09a133
BhuwanChandra <bhuwan7779@gmail.com>
2020-04-19 12:56:22 +0530

    created UI

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,10 +2,35 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+      <div className="home">
+        <div class="card home-card">
+          <h5>John</h5>
+          <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+          </div>
+          <div className="card-content">
+            <i className="small material-icons">favorite_border</i>
+            <i className="small material-icons">favorite</i>
+            <h6>title</h6>
+            <p>this is amazing post</p>
+            <input type="text" placeholder="add a comment" />
+          </div>
         </div>
-    )
+        <div class="card home-card">
+          <h5>John</h5>
+          <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+          </div>
+          <div className="card-content">
+            <i className="small material-icons">favorite_border</i>
+            <i className="small material-icons">favorite</i>
+            <h6>title</h6>
+            <p>this is amazing post</p>
+            <input type="text" placeholder="add a comment" />
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Home
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import '../App.css';
 
 function Profile() {
-    return (
-        <div>
-            <h1>profile</h1>
+  return (
+    <div style={{maxWidth: '600px', margin: '0px auto'}}>
+        <div style={{
+            display: "flex",
+            justifyContent: "space-around",
+            margin: "18px 0px",
+            borderBottom: '1px solid grey'
+        }}>
+            <div>
+                <img 
+                style={{ width: "160px", height: "160px", borderRadius: "50%" }}
+                src="https://images.unsplash.com/photo-1484186304838-0bf1a8cff81c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                />
+            </div>
+            <div>
+                <h4>John Doe</h4>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: "space-between",
+                    width: '108%'
+                    
+                }}>
+                    <h6>40 posts</h6>
+                    <h6>40 followers</h6>
+                    <h6>40 following</h6>
+                </div>
+            </div>
         </div>
-    )
+        <div className="gallery">
+            <img className="gallery-item" src="https://images.unsplash.com/photo-1484186304838-0bf1a8cff81c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="gallery-item" src="https://images.unsplash.com/photo-1484186304838-0bf1a8cff81c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="gallery-item" src="https://images.unsplash.com/photo-1484186304838-0bf1a8cff81c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="gallery-item" src="https://images.unsplash.com/photo-1484186304838-0bf1a8cff81c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="gallery-item" src="https://images.unsplash.com/photo-1484186304838-0bf1a8cff81c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="gallery-item" src="https://images.unsplash.com/photo-1484186304838-0bf1a8cff81c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+        </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
