c0b6a0329784b7b8dda2128870f9db0b725943fd
BhuwanChandra <bhuwan7779@gmail.com>
2020-04-22 16:17:07 +0530

    updated routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -62,11 +62,11 @@ router.post("/signin", (req, res) => {
         .then(doMatch => {
           if (doMatch) {
             const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
-            const { _id, name, email } = savedUser;
+            const { _id, name, email, followers, following } = savedUser;
             res.json({
               message: "Successfully signed in!!!",
               token,
-              user: {_id, name, email}
+              user: {_id, name, email, followers, following}
             });
           } else {
             return res
