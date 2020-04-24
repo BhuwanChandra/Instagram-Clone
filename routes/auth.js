a1c2bc98a269d3586cec8bc05d9f189fce17f315
BhuwanChandra <bhuwan7779@gmail.com>
2020-04-24 16:41:49 +0530

    bug fixed

--- routes/auth.js
+++ routes/auth.js
@@ -7,9 +7,6 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config/keys");
 const requireLogin = require('../middleware/requireLogin');
 
-router.get("/", (req, res) => {
-  res.send("<h1> Hello </h1>");
-});
 
 router.post("/signup", (req, res) => {
   const { name, email, password, pic } = req.body;
