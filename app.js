8af72916cc001e30c54648a355e04dedc49a453a
BhuwanChandra <bhuwan7779@gmail.com>
2020-04-24 16:29:42 +0530

    made changes for deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
-const { MONGO_URI } = require("./keys");
+const { MONGO_URI } = require("./config/keys");
 
 const User = require("./models/user");
 const Post = require("./models/post");
@@ -23,6 +23,15 @@ app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
+
+
+if(process.env.NODE_ENV == "production"){
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT} `);
